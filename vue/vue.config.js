030c64bfe9e9f70a80758296a05a316e29db72e7
mnbst <47024101+mnbst@users.noreply.github.com>
2021-06-06 13:54:59 +0900

    refactor

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -4,40 +4,21 @@ module.exports = {
     pages: {
         main_page: {
             entry: './src/main.js',
-            chunks: ['chunk-vendors']
         },
     },
     filenameHashing: false,
     productionSourceMap: false,
-    publicPath: process.env.NODE_ENV === 'production' ?
-        '' : 'http://localhost:8080/',
+    publicPath: 'http://localhost:8080/',
     outputDir: './public/webpack_bundles',
 
     chainWebpack: config => {
-
-        config.optimization
-            .splitChunks({
-                cacheGroups: {
-                    vendor: {
-                        test: /[\\/]node_modules[\\/]/,
-                        name: "chunk-vendors",
-                        chunks: "all",
-                        priority: 1
-                    },
-                },
-            });
-
         config
             .plugin('BundleTracker')
             .use(BundleTracker, [{
                 filename: './webpack/webpack-stats.json'
             }]);
 
-        config.resolve.alias
-            .set('__STATIC__', 'static');
-
         config.devServer
-            .public('http://localhost:8080')
             .host('0.0.0.0')
             .port(8080)
             .hotOnly(true)
@@ -45,9 +26,5 @@ module.exports = {
                 poll: 1000,
             })
             .https(false)
-            .headers({
-                "Access-Control-Allow-Origin": ["*"]
-            })
-
     },
 };
