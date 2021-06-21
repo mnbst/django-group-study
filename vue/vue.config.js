fecfb789957e1e0abfb31f7dc53f24242b1a77c2
mnbst <47024101+mnbst@users.noreply.github.com>
2021-06-21 15:56:41 +0900

    modify config

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,30 +1,31 @@
 const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
-    pages: {
-        main_page: {
-            entry: './src/main.js',
-        },
-    },
-    filenameHashing: false,
-    productionSourceMap: false,
-    publicPath: 'http://localhost:8080/',
-    outputDir: './public/webpack_bundles',
+  pages: {
+    main_page: {
+      entry: "./src/main.js"
+    }
+  },
+  filenameHashing: false,
+  productionSourceMap: false,
+  publicPath:
+    process.env.NODE_ENV === "production" ? "" : "http://localhost:8080/",
+  outputDir: "./public/webpack_bundles",
 
-    chainWebpack: config => {
-        config
-            .plugin('BundleTracker')
-            .use(BundleTracker, [{
-                filename: './webpack/webpack-stats.json'
-            }]);
+  chainWebpack: config => {
+    config.plugin("BundleTracker").use(BundleTracker, [
+      {
+        filename: "./webpack/webpack-stats.json"
+      }
+    ]);
 
-        config.devServer
-            .host('0.0.0.0')
-            .port(8080)
-            .hotOnly(true)
-            .watchOptions({
-                poll: 1000,
-            })
-            .https(false)
-    },
+    config.devServer
+      .host("localhost")
+      .port(8080)
+      .hotOnly(true)
+      .watchOptions({
+        poll: 1000
+      })
+      .https(false);
+  }
 };
